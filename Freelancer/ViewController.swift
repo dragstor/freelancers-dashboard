517308ac2145ebda1890b291bba91509593e754e
Nikola Stojkovic <dragstor@gmail.com>
2019-02-04 01:42:10 +0100

    Initial Commit

---
+++ Freelancer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Freelancer
+//
+//  Created by Nikola on 2/4/19.
+//  Copyright © 2019 Stojković. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
