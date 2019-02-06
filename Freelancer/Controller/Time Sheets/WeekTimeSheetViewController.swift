6910fda02318bc0d54ab7f4349dfa0174bfa4769
Nikola Stojkovic <dragstor@gmail.com>
2019-02-06 17:25:25 +0100

    Added Week Timesheet view controller

--- Freelancer/Controller/Time Sheets/WeekTimeSheetViewController.swift
+++ Freelancer/Controller/Time Sheets/WeekTimeSheetViewController.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import Cocoa
+
+class WeekTimeSheetViewController: NSViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    
+}
